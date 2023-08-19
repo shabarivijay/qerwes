c116ee75e26b8eaf2fe1aa7993095d3d08c62e80
shabarivijay <shabari.vijay@gmail.com>
2023-08-19 11:18:30 +0530

    today push

---
+++ src/test/java/wewet/hghjghghqa.java
@@ -0,0 +1,15 @@
+package wewet;
+
+import org.testng.annotations.Test;
+
+public class hghjghghqa
+{
+	public class awdfsd 
+	{
+	    @Test
+	    public void abc()
+	    {
+	    	System.out.println("hello");
+	    }
+	}
+}
